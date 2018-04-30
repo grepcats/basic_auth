1357a70d5abff495f9105143159fe17646b6ec5d
Kayla Ondracek and Jasun Feddema <kayla.renee@gmail.com and jasun.feddema@gmail.com>
2018-04-30 10:39:05 -0700

    add ability to add items to user's to do list

--- BasicAuthentication/Migrations/20180430173515_AddItemTable.Designer.cs
+++ BasicAuthentication/Migrations/20180430173515_AddItemTable.Designer.cs
@@ -8,9 +8,10 @@ using BasicAuthentication.Models;
 namespace BasicAuthentication.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180430173515_AddItemTable")]
+    partial class AddItemTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.2");
